922028f5d285c44037e1fa53aa379c61f7a23e4e
mostafamahmood17 <taat.heritage@gmail.com>
2020-09-30 15:25:03 -0700

    root added

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 5000;
 
+app.get('/', (req,res)=>{
+    res.send("hello port is running fine")
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
 client.connect(err => {
